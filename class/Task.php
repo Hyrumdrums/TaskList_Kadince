8a28f1fc0d9fc70e67e33b3f462e3b897fc15aae
Dallin <Hyrumdrums@gmail.com>
2021-11-21 23:29:00 -0700

    na

--- class/Task.php
+++ class/Task.php
@@ -21,7 +21,7 @@ class Task extends DataObject
 			FROM Task
 			$where
 MYSQL;
-		$this->stmt = $this->ExecuteSQL($sql, __FUNCTION__, $parameterList);
+		$this->stmt = $this->ExecuteSQL($sql, __FUNCTION__, $filterList);
 		return $this->GetObjectList($this->stmt);
 	}
 }
