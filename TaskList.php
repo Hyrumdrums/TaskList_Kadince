4b5feb9e4b9c15153e1f62da9041cc557f4d3382
Dallin <Hyrumdrums@gmail.com>
2021-11-22 08:18:21 -0700

    na

--- TaskList.php
+++ TaskList.php
@@ -21,7 +21,7 @@
 		$onclick = "EditTask($id);";
 		$class = $task->Get('Status');
 		$items .= <<<HTML
-			<li class="$class" onclick="$onclick">$desc</li><button onclick="Delete($id);">x</button>
+			<li class="$class" onclick="$onclick">$desc<button onclick="Delete($id);">x</button></li>
 HTML;
 	}
 	$taskList = <<<HTML
