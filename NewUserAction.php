a923d7951f5ca3fc68944db25e0f4beee5c286b5
Dallin <Hyrumdrums@gmail.com>
2021-11-22 07:50:32 -0700

    na

--- NewUserAction.php
+++ NewUserAction.php
@@ -6,7 +6,7 @@
 	$username = GetPost('username');
 	$password = GetPost('password');
 	$password2 = GetPost('password2');
-	if($password1 <> $password2) Route::To('index', 'NewUser', array('Err'=>'passwords must match'));
+	if($password <> $password2) Route::To('index', 'NewUser', array('Err'=>'passwords must match'));
 	if(!$username || !$password) Route::To('index', 'NewUser', array('Err'=>'please enter username and password'));
 	if(!User::Create($username, $password)) Route::To('NewUser', '', array('Err'=>'try a different username'));
 	Route::To('index');
